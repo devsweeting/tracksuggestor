a0f8e2e5cf7e0d50817c0afcd7f062d12ad3b763
Devin Sweeting <devinsweeting@gmail.com>
2018-10-19 13:21:44 -0700

    add questions 4 & 5

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,15 @@ $(document).ready(function() {
     var workplaceInput = $("input:radio[name=workplace-type]:checked").val();
     var sandwichInput = $("input:radio[name=sandwich-type]:checked").val();
     var freetimeInput = $("input:radio[name=freetime-type]:checked").val();
+    var salaryInput =
+    $("input:radio[name=salary-type]:checked").val();
+    var interestInput =
+    $("input:radio[name=interest-type]:checked").val();
 
     var csharp = 0;
     var ruby= 0;
     var design = 0;
-// question 1
+// workplace
     if (workplaceInput === "Corporation") {
       csharp += 1;
     } else if (workplaceInput === 'Start-up') {
@@ -17,7 +21,7 @@ $(document).ready(function() {
     } else if (workplaceInput === 'CreativeAgency') {
       design+= 1;
     }
-// question 2
+// sandwich
     if (sandwichInput === "toast") {
       csharp += 1;
     } else if (sandwichInput === 'artisan') {
@@ -25,7 +29,7 @@ $(document).ready(function() {
     } else if (sandwichInput === 'home') {
       design += 1;
     }
-// question 3
+// freetime
     if (freetimeInput === "netflix") {
       csharp += 1;
     } else if (freetimeInput === 'adventure') {
@@ -33,6 +37,24 @@ $(document).ready(function() {
     } else if (freetimeInput === 'art') {
       design += 1;
     }
+
+// salary
+    if (salaryInput === "midrange") {
+      csharp += 1;
+    } else if (salaryInput === 'highrange') {
+      ruby += 1;
+    } else if (salaryInput === 'lowrange') {
+      design += 1;
+    }
+// Interest
+        if (interestInput === "employment") {
+          csharp += 1;
+        } else if (interestInput === "income") {
+          ruby += 1;
+        } else if (interestInput === "onlineart") {
+          design += 1;
+        }
+
 // Results
     $(".suggestedtrack").show();
     if (csharp > ruby+design) {
